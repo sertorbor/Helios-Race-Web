643a277810f93b94ac99422f71f277fbe745fbc1
DGRUESOC <DGRUESOC@ford.com>
2025-02-28 10:11:47 +0100

    Estructurados miembros en page About

--- client/src/pages/about.tsx
+++ client/src/pages/about.tsx
@@ -146,7 +146,7 @@ export default function About() {
             transition={{ duration: 0.6 }}
             className="mt-16 text-center h-[600px] w-full"
           >
-            <h2 className="text-3xl md:text-4xl font-bold mb-4 my-[100px] font-permanent-marker-regular">{t("about.competition.title")}</h2>
+          <h2 className="text-3xl md:text-4xl font-bold mb-4 my-[100px] font-permanent-marker">{t("about.competition.title")}</h2>            
             <div className="grid grid-cols-1 md:grid-cols-2 gap-1 my-[100px]">
               <div className="text-center">
                 <img
